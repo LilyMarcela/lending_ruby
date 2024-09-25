5679a8b993b7d34cf49afdbc63fb5da4dfcd59e3
marcela blanco <marcela.b.abril@gmail.com>
2024-09-25 15:51:21 -0500

    Added Devise user authentication with role-based access control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+
+
+    def authenticate_borrower!
+      redirect_to root_path, alert: "Go away"
+    end
+
+    def authenticate_lender
+      redirect_to root_path, alert: "Go away"
+    end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  enum role: { borrower: 0, lender: 1}
+end
